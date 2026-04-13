90908c93644ffbda5388bff9bb3c0e797195db49
rustleupchef <rustle.up.chef@gmail.com>
2026-04-12 22:26:07 -0500

    added date handling inside the site

--- static/index/script.js
+++ static/index/script.js
@@ -36,4 +36,28 @@ function write() {
     writer.innerHTML = currentText + "_";
 }
 
-setInterval(write, 100);
+
+function getNextMeeting(refDate, today) {
+    const msPerDay = 24 * 60 * 60 * 1000;
+    const daysInBetween = 14;
+    const daysSinceRef = Math.floor((today - refDate) / msPerDay);
+    const daysUntilNext = daysInBetween - (daysSinceRef % daysInBetween);
+    const nextMeeting = new Date(today.getTime() + (daysUntilNext * msPerDay));
+    return nextMeeting;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    setInterval(write, 100);
+    const url = `https://sheets.wais-cshs.workers.dev/Schedule`;
+    fetch(url)
+        .then(res => res.json())
+        .then(cells => {
+            const today = new Date(new Date().toDateString());
+            const firstMeeting = new Date(cells.values[1][0]);
+            const nextMeetingDate = getNextMeeting(firstMeeting, today);
+            const daysUntilMeeting = Math.ceil((nextMeetingDate - today) / (1000 * 60 * 60 * 24));
+
+            const textBox = document.querySelector(".text-box");
+            textBox.innerHTML += `The next meeting will be ${nextMeetingDate.toLocaleDateString()} meaning ${daysUntilMeeting} day(s) till then`
+        });
+});
